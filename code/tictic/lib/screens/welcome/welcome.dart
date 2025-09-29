91d3051162e98108daa79b44ad1c9afa69dc5ca0
schmitz-eline <eline.schmitz@student.hepl.be>
2025-09-29 16:51:29 +0200

    Add logo to welcome screen

--- code/tictic/lib/screens/welcome/welcome.dart
+++ code/tictic/lib/screens/welcome/welcome.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:tictic/constants/sizes.dart';
 import 'package:tictic/screens/welcome/widgets/logo_welcome.dart';
 
 class Welcome extends StatelessWidget {
