977a869f2efe45aee657b9b089792a49d850ecaf
schmitz-eline <eline.schmitz@student.hepl.be>
2025-10-06 16:55:54 +0200

    Refactor registration and welcome screens to use ScaffoldWithImage for consistent background styling

--- code/tictic/lib/screens/welcome/welcome.dart
+++ code/tictic/lib/screens/welcome/welcome.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:tictic/screens/scaffold_with_image.dart';
 import 'package:tictic/screens/welcome/widgets/call_to_actions.dart';
 import 'package:tictic/screens/welcome/widgets/logo_welcome.dart';
 import 'package:tictic/screens/welcome/widgets/text_slider_with_bullets.dart';
-
-import '../../constants/colors.dart';
 
 class Welcome extends StatelessWidget {
   const Welcome({super.key});
@@ -12,27 +11,16 @@ class Welcome extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/img/back1.png"),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: SafeArea(
-          child: Column(
-            children: [
-              Spacer(),
-              LogoWelcome(),
-              Spacer(),
-              TextSliderWithBullets(),
-              Spacer(),
-              CallToActions(),
-            ],
-          ),
-        ),
+    return ScaffoldWithImage(
+      child: Column(
+        children: [
+          Spacer(),
+          LogoWelcome(),
+          Spacer(),
+          TextSliderWithBullets(),
+          Spacer(),
+          CallToActions(),
+        ],
       ),
     );
   }
